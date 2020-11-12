4f7870e9ce5bf25932e95ac76c258ad2cbc1a913
hp <hp_ctt@hotmail.com>
2020-11-12 15:57:37 +0000

    add pt and en lang with sitemap

--- pages/index.vue
+++ pages/index.vue
@@ -2,7 +2,11 @@
   <div class="main text-center flex flex-col items-center justify-center">
     <!-- <logo /> -->
     <h1 class="title">Nuxt — Tailwind — Netlify CMS</h1>
-    <h2 class="subtitle">Boilerplate</h2>
+    <h2 class="subtitle">Boilerplate - {{ $t('welcome') }}</h2>
+
+    <nuxt-link v-for="locale in availableLocales" :key="locale.code" :to="switchLocalePath(locale.code)">
+      {{ locale.name }}
+    </nuxt-link>
   </div>
 </template>
 
@@ -13,10 +17,16 @@ export default {
   // components: {
   //   Logo
   // },
+  name: 'index',
   head() {
     return {
-      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }]
+      script: [{ src: 'https://identity.netlify.com/v1/netlify-identity-widget.js' }],
     }
-  }
+  },
+  computed: {
+    availableLocales() {
+      return this.$i18n.locales.filter((i) => i.code !== this.$i18n.locale)
+    },
+  },
 }
 </script>
